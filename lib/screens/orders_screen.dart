08321e66f3da897613c9486b22ec98cf7444c3b0
El-rhezzali Manal <manal.gzl99@gmail.com>
2021-04-25 11:41:22 +0000

    using FutureBuilder with a statelessWidget to avoid having a statefulWidget just to use the initState method

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,43 +5,47 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = "/orders";
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
+    // final ordersData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Orders"),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: ordersData.orders.length,
-              itemBuilder: (ctx, index) => OrderItem(ordersData.orders[index]),
-            ),
+      //to avoid using a StatefulWidget just for using initState()
+      //but if we have another state using like this will cause a 
+      //new Future object initialization with every build re-run
+      //dataSnapshot is the data currently returned by the Future
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          // means we are currently loading
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            //we got an error while loading
+            if (dataSnapshot.error != null) {
+              //handling the error
+              //.................
+              return Center(
+                child: Text("An error occurred!"),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, index) =>
+                      OrderItem(orderData.orders[index]),
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
