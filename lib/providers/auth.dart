3e7b58c2b7ebd44cb877467745610f3091ae0166
El-rhezzali Manal <manal.gzl99@gmail.com>
2021-04-26 03:10:19 +0000

    handling authentification errors

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
+
 class Auth with ChangeNotifier {
   //this token expires for example the token that firebase generates
   //expires after 1 hour
@@ -11,7 +14,7 @@ class Auth with ChangeNotifier {
   String _userId;
 
   Future<void> signup(String email, String password) async {
-    _authenticate(email, password, "signUp");
+    return _authenticate(email, password, "signUp");
     // final url = Uri.parse(
     //     "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCqorfyo_wZX6ajvh7T1eDc6ETfEkinOoU");
     // final response = await http.post(
@@ -29,19 +32,26 @@ class Auth with ChangeNotifier {
       String email, String password, String urlSegment) async {
     final url = Uri.parse(
         "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCqorfyo_wZX6ajvh7T1eDc6ETfEkinOoU");
-    final response = await http.post(
-      url,
-      body: json.encode({
-        "email": email,
-        "password": password,
-        "returnSecureToken": true,
-      }),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          "email": email,
+          "password": password,
+          "returnSecureToken": true,
+        },),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData["error"] != null) {
+        throw HttpException(responseData["error"]["message"]);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> login(String email, String password) async {
-    _authenticate(email, password, "signInWithPassword");
+     return _authenticate(email, password, "signInWithPassword");
     //  final url = Uri.parse(
     //     "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCqorfyo_wZX6ajvh7T1eDc6ETfEkinOoU");
     // final response = await http.post(
