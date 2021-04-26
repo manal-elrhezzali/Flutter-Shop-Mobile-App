e829d802e23e555cf78623fb3ba74267c66bfe96
El-rhezzali Manal <manal.gzl99@gmail.com>
2021-04-26 15:02:09 +0000

    managing the auth token locally

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// import './screens/products_overview_screen.dart';
+import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
-
 
 import './providers/products.dart';
 import './providers/cart.dart';
@@ -42,21 +41,25 @@ class MyApp extends StatelessWidget {
           create: (ctx) => Auth(),
         ),
       ],
-      child: MaterialApp(
-        title: 'MNÉL\'s Shop',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: "Lato",
+      child: Consumer<Auth>(
+        //auth : latest auth object
+        //builder runs whenever the auth object changes
+        builder: (ctx, auth, _) => MaterialApp(
+          title: 'MNÉL\'s Shop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: "Lato",
+          ),
+          home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => CartScreen(),
+            OrdersScreen.routeName: (ctx) => OrdersScreen(),
+            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+            EditProductScreen.routeName: (ctx) => EditProductScreen(),
+          },
         ),
-        home: AuthScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          CartScreen.routeName: (ctx) => CartScreen(),
-          OrdersScreen.routeName: (ctx) => OrdersScreen(),
-          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-          EditProductScreen.routeName: (ctx) => EditProductScreen(),
-        },
       ),
     );
   }
--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -5,13 +5,25 @@ import 'package:http/http.dart' as http;
 
 import '../models/http_exception.dart';
 
-
 class Auth with ChangeNotifier {
   //this token expires for example the token that firebase generates
   //expires after 1 hour
   String _token;
   DateTime _expiryDate;
   String _userId;
+
+  bool get isAuth {
+    return token != null;
+  }
+
+  String get token {
+    if (_expiryDate != null &&
+        _expiryDate.isAfter(DateTime.now()) &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+  }
 
   Future<void> signup(String email, String password) async {
     return _authenticate(email, password, "signUp");
@@ -35,23 +47,31 @@ class Auth with ChangeNotifier {
     try {
       final response = await http.post(
         url,
-        body: json.encode({
-          "email": email,
-          "password": password,
-          "returnSecureToken": true,
-        },),
+        body: json.encode(
+          {
+            "email": email,
+            "password": password,
+            "returnSecureToken": true,
+          },
+        ),
       );
       final responseData = json.decode(response.body);
       if (responseData["error"] != null) {
         throw HttpException(responseData["error"]["message"]);
       }
+      //there is no error in the response data
+      _token = responseData["idToken"];
+      _userId = responseData["localId"];
+      _expiryDate = DateTime.now()
+          .add(Duration(seconds: int.parse(responseData["expiresIn"])));
+      notifyListeners();
     } catch (error) {
       throw error;
     }
   }
 
   Future<void> login(String email, String password) async {
-     return _authenticate(email, password, "signInWithPassword");
+    return _authenticate(email, password, "signInWithPassword");
     //  final url = Uri.parse(
     //     "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCqorfyo_wZX6ajvh7T1eDc6ETfEkinOoU");
     // final response = await http.post(
